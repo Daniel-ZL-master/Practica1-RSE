f3b8eee014adb9f6295f4a5c96634cc45fe3cf6e
Daniel <danielzorlej@gmail.com>
2026-02-13 13:26:40 +0100

    Actualización tras comprobar en osciloscopio

--- Hito3/Hito3.ino
+++ Hito3/Hito3.ino
@@ -1,46 +1,23 @@
-unsigned long tprev_ON;
-unsigned long tprev_OFF;
-int t_ON;
-int t_OFF;
-float duty;
-int estado = 0;
+#include <Arduino.h>
+#include <mbed.h> // Necesario para acceder a las funciones nativas del nRF52
+
+// Creamos un objeto PwmOut.
+mbed::PwmOut pwm_pin(digitalPinToPinName(2));
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(2, OUTPUT);
+  // T = 1 / 5000 = 0.0002 segundos
+  pwm_pin.period(0.0002f); 
+  
+  // Inicializamos en 0
+  pwm_pin.write(0.0f);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   int raw_adc = analogRead(A0);
-  int voltaje = raw_adc * 3300 / 1024;
-  unsigned long tactual = micros();
-  duty = (float)voltaje / 3300.0;
-  t_ON = duty * 200;
-  t_OFF = 200 - t_ON;
-  switch (estado) {
-    case 0:
-      digitalWrite(2, HIGH);
-      tprev_ON = tactual;
-      estado = 1;
-      break;
-    case 1:
-      Serial.println(1);
-      if (tactual - tprev_ON >= t_ON) {
-        estado = 2;
-      }
-      break;
-    case 2:
-      digitalWrite(2, LOW);
-      tprev_OFF = tactual;
-      estado = 3;
-      break;
-    case 3:
-      Serial.println(0);
-      if (tactual - tprev_OFF >= t_OFF) {
-        estado = 0;
-      }
-      break;
-  }
+
+  float duty = raw_adc / 1023.0;
+
+  pwm_pin.write(duty);
+  delay(10);
 }
